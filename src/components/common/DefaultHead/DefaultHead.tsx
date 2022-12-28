b1b63b11690d3fcb3b1c89a340f79b267859a46f
Jaechan-So <jaechan331@gmail.com>
2022-12-28 13:30:33 +0900

    feat: add Open Graph

--- src/components/common/DefaultHead/DefaultHead.tsx
+++ src/components/common/DefaultHead/DefaultHead.tsx
@@ -1,5 +1,7 @@
 import Head from 'next/head';
 
+import OpenGraphImage from '~/public/jaechan-so-github-io-og-image.png';
+
 const DefaultHead = () => (
   <Head>
     <title>Jaechan-So.github.io</title>
@@ -8,6 +10,10 @@ const DefaultHead = () => (
       content="Hi! My name is Jaechan So. I'm currently working as a Software Engineer in South Korea."
     />
     <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <meta property="og:title" content="Jaechan-So.github.io" />
+    <meta property="og:type" content="website" />
+    <meta property="og:url" content="https://jaechan-so.github.io/" />
+    <meta property="og:image" content={OpenGraphImage.src} />
   </Head>
 );
 
