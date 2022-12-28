aba464d371cf8c4801e422de681688344a8ec3b5
Jaechan-So <jaechan331@gmail.com>
2022-12-28 13:11:58 +0900

    Deploy

---
+++ out/_next/static/I52YKrGVrGTlkMF8FjeWz/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/39-52e523850f7216d3.js","static/css/c4d887d8349a1336.css","static/chunks/pages/index-673366bdf2f8ae72.js"],"/_error":["static/chunks/pages/_error-d17ef67ced0190f0.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
