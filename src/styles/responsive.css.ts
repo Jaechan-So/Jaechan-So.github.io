76d16ed20b52ae1db764684b4a88a44b33d7ae34
Jaechan-So <jaechan331@gmail.com>
2022-12-29 23:57:57 +0900

    feat: add responsive recipe utility function

--- src/styles/responsive.css.ts
+++ src/styles/responsive.css.ts
@@ -1,5 +1,10 @@
 /* eslint-disable sort-keys, sort-keys-fix/sort-keys-fix */
-import { ComplexStyleRule, style } from '@vanilla-extract/css';
+import {
+  ComplexStyleRule,
+  StyleRule,
+  style,
+  styleVariants,
+} from '@vanilla-extract/css';
 
 const breakPoints = {
   mobile: 768,
@@ -38,3 +43,78 @@ export const responsiveStyle = (props: ResponsiveStyleProps) =>
     ),
     ...props.desktop,
   });
+
+type ResponsiveRecipeProps<
+  StyleMap extends Record<string, StyleRule>,
+  Variants extends Record<
+    string,
+    { [K in keyof typeof breakPoints]?: keyof StyleMap | (keyof StyleMap)[] }
+  >,
+> = {
+  baseStyle?: string;
+  styleMap: StyleMap;
+  variants: Variants;
+};
+
+export const responsiveRecipe = <
+  StyleMap extends Record<string, StyleRule>,
+  Variants extends Record<
+    string,
+    { [K in keyof typeof breakPoints]?: keyof StyleMap | (keyof StyleMap)[] }
+  >,
+>({
+  baseStyle,
+  styleMap,
+  variants,
+}: ResponsiveRecipeProps<StyleMap, Variants>) => {
+  const rules = {} as Record<keyof Variants, ComplexStyleRule>;
+
+  (
+    Object.entries(variants) as [
+      keyof Variants,
+      { [K in keyof typeof breakPoints]?: keyof StyleMap | (keyof StyleMap)[] },
+    ][]
+  ).forEach(([varName, varMap]) => {
+    let compositeResponsiveStyle: StyleRule = {};
+    const mediaQueries: Record<string, StyleRule> = {};
+
+    (
+      Object.entries(varMap) as [
+        keyof typeof varMap,
+        keyof StyleMap | (keyof StyleMap)[],
+      ][]
+    ).forEach(([breakPoint, styleName]) => {
+      const mediaQueryKey = mediaQueryTargets[breakPoint];
+      let compositeStyle: StyleRule = {};
+
+      if (Array.isArray(styleName)) {
+        styleName.forEach((name) => {
+          compositeStyle = { ...compositeStyle, ...styleMap[name] };
+        });
+      } else {
+        compositeStyle = styleMap[styleName];
+      }
+
+      if (mediaQueryKey) {
+        mediaQueries[mediaQueryKey] = compositeStyle;
+      } else {
+        compositeResponsiveStyle = {
+          ...compositeResponsiveStyle,
+          ...compositeStyle,
+        };
+      }
+    });
+
+    compositeResponsiveStyle = {
+      ...compositeResponsiveStyle,
+      '@media': mediaQueries,
+    };
+
+    rules[varName] = [
+      ...(baseStyle ? [baseStyle] : []),
+      compositeResponsiveStyle,
+    ];
+  });
+
+  return styleVariants(rules);
+};
