f162dd1ef246fe266eb610fd3aae78c8002005b3
Jaechan-So <jaechan331@gmail.com>
2022-12-30 00:01:12 +0900

    fix: remove default meta tag from root App component

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,5 @@
 import type { AppProps } from 'next/app';
 
-import { DefaultHead } from '@/components/common/DefaultHead';
 import { themeClass } from '@/styles/theme.css';
 
 import '@/styles/global.css';
@@ -8,12 +7,9 @@ import 'the-new-css-reset/css/reset.css';
 
 const App = ({ Component, pageProps }: AppProps) => {
   return (
-    <>
-      <DefaultHead />
-      <div className={themeClass}>
-        <Component {...pageProps} />
-      </div>
-    </>
+    <div className={themeClass}>
+      <Component {...pageProps} />
+    </div>
   );
 };
 
